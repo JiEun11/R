65f5d6e3a7e46b8f9a28656058f78d234192f3d7
Bella Jin <shdudtnr3939@naver.com>
2020-11-01 21:44:50 +0100

    task4 complete!

--- task4/task4Script.R
+++ task4/task4Script.R
@@ -1,4 +1,6 @@
-load("~/Documents/R/task/R/task4/task4.rda")
+#loading packages
+library(fitdistrplus)
+
 club_mat = as.matrix(club_ad)
 #sum of degrees
 sumDegreeNode = apply(club_mat, 1, sum)
@@ -7,6 +9,26 @@ min(sumDegreeNode)
 max(sumDegreeNode)
 mean(sumDegreeNode)
 #frequency
-dfsumDegreeNode = as.data.frame(sumDegreeNode)
+dfsumDegreeNode <- as.data.frame(sumDegreeNode)
 #relative frequency
 rfreqDfSumDegreeNode <- sumDegreeNode/length(sumDegreeNode)
+#change to dataframe
+dfrFreqDFSumDegreeNode <- as.data.frame(rfreqDfSumDegreeNode)
+#histogram about frequency
+hist(sumDegreeNode, breaks = (max(sumDegreeNode)-min(sumDegreeNode))*5, probability = T, col="grey")
+#histogram about relative frequency
+hist(rfreqDfSumDegreeNode, breaks = (max(rfreqDfSumDegreeNode)-min(rfreqDfSumDegreeNode))*5, probability = T, col="grey")
+hist(rfreqDfSumDegreeNode)
+
+#empirical cumulative distribution function
+plot(ecdf(sumDegreeNode))
+
+#using the plotdist() function from the fitdistrplus package
+plotdist(sumDegreeNode, histo = TRUE, demp = TRUE)
+
+
+
+
+
+
+
